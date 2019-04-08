19c68804b6dd84399341ddd106039d7ceaa0b31f
Raquel <raquelmartins203@yahoo.com.br>
2019-04-08 20:41:10 -0200

    1

--- lib/lista_disciplinas.dart
+++ lib/lista_disciplinas.dart
@@ -1,52 +1,31 @@
 import 'package:flutter/material.dart';
 import './add_disciplina.dart';
+import './view_disciplina.dart';
 
 class ListaDisciplinas extends StatefulWidget {
 //   final Container container;
   final List<Container> lista;
-  ListaDisciplinas({
+  ListaDisciplinas(
+      {
 //     this.container,
-   this.lista
-   }); 
-  
-    @override
-    State createState() => new _ListaDisciplinas();
+      this.lista});
+
+  @override
+  State createState() => new _ListaDisciplinas();
 }
 
 class _ListaDisciplinas extends State<ListaDisciplinas> {
- 
   final String _disciplina = "", _cod = "";
   final double _meta = 0.0;
 
   @override
-  Widget build(BuildContext context) { 
-        if (widget.lista.isEmpty) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Organizer"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-          
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return FormDisciplina(lista: widget.lista);
-            }));
-          },
-          child: Icon(Icons.add),
-          backgroundColor: Colors.green[400],
-          tooltip: "Adicionar Disciplina",
-        ),
-        body: new Container()
-      );
-    } else  {
+  Widget build(BuildContext context) {
     return Scaffold(
-        
         appBar: AppBar(
           title: Text("Organizer"),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-          
             Navigator.push(context, MaterialPageRoute(builder: (context) {
               return FormDisciplina(lista: widget.lista);
             }));
@@ -58,13 +37,14 @@ class _ListaDisciplinas extends State<ListaDisciplinas> {
         body: new Container(
             child: new Column(
           children: <Widget>[
-            
+            txtListaVazia(widget.lista.length),
             new Expanded(
-              
               child: ListView.builder(
                 itemCount: widget.lista.length,
                 itemBuilder: (BuildContext context, int index) {
-     
+                  disableTxtVazia(
+                    txtListaVazia(widget.lista.length),
+                  );
                   return widget.lista[index];
                 },
               ),
@@ -72,7 +52,19 @@ class _ListaDisciplinas extends State<ListaDisciplinas> {
           ],
         )));
   }
+}
+
+Container txtListaVazia(int tam) {
+  if (tam == 0) {
+    return null;
+  } else {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("Não existe disciplinas cadastradas!"),
+    );
   }
 }
 
- 
+Visibility disableTxtVazia(Container cont) {
+  return Visibility(child: cont, visible: false);
+}
