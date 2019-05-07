999b235f3b0d66fecf2f4c2ea7d7f1867d4388eb
Raquel <raquelmartins203@yahoo.com.br>
2019-05-07 11:30:56 -0200

    1

--- lib/view/lista_tarefas.dart
+++ lib/view/lista_tarefas.dart
@@ -20,7 +20,6 @@ class ListaTarefas extends StatefulWidget {
 }
 
 class _ListaTarefas extends State<ListaTarefas> {
-  int count = 0;
   DatabaseHelper databaseHelper = DatabaseHelper();
 
   @override  
@@ -33,7 +32,83 @@ class _ListaTarefas extends State<ListaTarefas> {
     return Scaffold( 
       appBar: AppBar(  
         title: Text("Tarefas"),
+        backgroundColor: Colors.purple[300],
+        actions: <Widget>[ 
+         FlatButton(  
+           child: Icon(Icons.remove_red_eye),
+           onPressed: () => {},
+          //   Navigator.push(context, MaterialPageRoute(builder (context) {
+          //     return 
+          //  })),
+         )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(  
+        onPressed: () => {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green[400],
+        tooltip: "Adicionar Tarefa",
+      ),
+      body: Container(
+        child: Column(children: <Widget>[  
+          txtListaVazia(widget.listaTarefa.length),
+          carregarLista()
+        ],),),
+    );
+  }
+
+  Container txtListaVazia(int tam) {
+    if (tam==0){
+      return Container(  
+        padding: EdgeInsets.only(top:260.0),
+        alignment: Alignment.center,
+        child: Text(  
+          "Não há tarefas cadastradas!",
+          style: TextStyle(color: Colors.grey[600], fontSize: 18.0),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  //getTarefaListView
+  Expanded carregarLista() {
+    return Expanded(  
+      child: ListView.builder(  
+        itemCount: widget.listaTarefa.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container (  
+            child: Card(  
+              child: ListTile(  
+                leading: Icon(Icons.warning, size: 40.0),
+                title: Text(widget.listaTarefa[index].getDisciplina()),
+                subtitle: Text(widget.listaTarefa[index].getTipo()),
+                trailing: Text(widget.listaTarefa[index].getNota().toString()+"/"+
+                widget.listaTarefa[index].getValor().toString()),
+                onTap: () => {},
+
+              ),
+            ),
+          );
+        },
       ),
     );
+  }
+
+  Visibility disableTxtVazia(Container cont) {
+    return Visibility(child: cont, visible: false,);
+  }
+
+  void atualizarListView() {
+    final Future<Database> dbFuture = databaseHelper.iniciarDb();
+    dbFuture.then((database) {
+      Future<List<Tarefa>> tarefaListFUture = databaseHelper.getTarefaLista();
+      tarefaListFUture.then((listaTarefa) {
+        setState(() {
+         listaTarefa = widget.listaTarefa;
+        });
+      });
+    });
   }
 }
